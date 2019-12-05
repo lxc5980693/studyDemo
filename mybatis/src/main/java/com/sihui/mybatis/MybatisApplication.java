d28904ce1e7de1d87a486c307ceb87ba36980c02
lixuanchen <lixuanchen@souche.com>
2019-12-05 17:10:14 +0800

    mybatis配置文件修改代码开发

--- mybatis/src/main/java/com/sihui/mybatis/MybatisApplication.java
+++ mybatis/src/main/java/com/sihui/mybatis/MybatisApplication.java
@@ -1,12 +1,28 @@
 package com.sihui.mybatis;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.sihui.mybatis.mapper.UserMapper;
+import com.sihui.mybatis.model.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 public class MybatisApplication {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream resourceAsStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory build =
+                new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = build.openSession(ExecutorType.SIMPLE);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = mapper.selectAll();
+        System.out.println(users.toString());
     }
 
 }
--- mybatis/src/test/java/com/sihui/mybatis/MybatisApplicationTests.java
+++ mybatis/src/test/java/com/sihui/mybatis/MybatisApplicationTests.java
@@ -1,12 +1,7 @@
 package com.sihui.mybatis;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class MybatisApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
