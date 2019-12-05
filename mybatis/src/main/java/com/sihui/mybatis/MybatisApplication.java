df37c62c87197a9a0a539dc556526a67b6843855
lixuanchen <lixuanchen@souche.com>
2019-12-05 15:48:48 +0800

    mybatis源码研究项目

---
+++ mybatis/src/main/java/com/sihui/mybatis/MybatisApplication.java
@@ -0,0 +1,12 @@
+package com.sihui.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class MybatisApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
